9404aa424b8309215a6e48a36281f01c18706855
c0b23137 <c0b2313715@edu.teu.ac.jp>
2024-10-08 16:12:49 +0900

    課題１：score表示

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -141,6 +141,33 @@ class Bomb:
         screen.blit(self.img, self.rct)
 
 
+class Score:
+    """
+    スコアに関するクラス
+    """
+    def __init__(self):
+        """
+        フォントを設定
+        文字列Surfaceを生成する
+        """
+        self.fonto = pg.font.SysFont("hgp創英角ポップ体",30)
+        self.coler=(0,0,255)
+        self.score=0
+        self.img = self.fonto.render(f"{self.score}",0,self.coler)
+        self.center=(100,HEIGHT-50)
+
+    def update(self,screen:pg.surface,get=0):
+        """
+        現在のスコアを生成
+        スクリーンbilt
+        """
+        self.score+=get
+        self.img=self.fonto.render(f"{self.score}",0,self.coler)
+        self.rct=self.img.get_rect()
+        self.rct.center=(self.center)
+        screen.blit(self.img,self.rct)
+
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
@@ -148,6 +175,7 @@ def main():
     bird = Bird((300, 200))
     # bomb = Bomb((255, 0, 0), 10)
     bombs =[Bomb((255,0,0),10) for _ in range(NUM_OB_BOMBS)]
+    score = Score()
     clock = pg.time.Clock()
     tmr = 0
     beam=None
@@ -177,6 +205,8 @@ def main():
                 if beam.rct.colliderect(bomb.rct): #  ビームと爆弾が衝突したら
                     beam,bombs[j] =None,None
                     bird.change_img(6,screen)
+                    #print(score)
+                    score.update(screen,get=1)
                     pg.display.update()
         bombs =[bomb for bomb in bombs if bomb is not None]
 
@@ -186,6 +216,7 @@ def main():
             beam.update(screen)   
         for bomb in bombs:
             bomb.update(screen)
+        score.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
